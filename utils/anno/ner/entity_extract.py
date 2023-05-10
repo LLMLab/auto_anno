fd043db2140496b5ddf8dfe1f3f5ef5d0343a432
maxmon <541182180@qq.com>
2023-05-11 00:42:52 +0800

    style(NER): 去除无关注释

--- utils/anno/ner/entity_extract.py
+++ utils/anno/ner/entity_extract.py
@@ -58,16 +58,3 @@ if __name__ == '__main__':
     # extract_named_entities("```汤姆每天都被杰瑞欺负，皮卡丘越来越想帮忙，竟然还总是被拒绝，心想难道我“皮大仙”这点能力都没有？而且，这货不是被虐狂吧```", ["Person", "物种"])
     result = extract_named_entities('老百姓心新乡新闻网话说这几天新乡天气还好吧偷笑', ['代称', '行政区'])
     print(result)
-    # Tags: PER(人名), LOC(地点名), GPE(行政区名), ORG(机构名)
-    # Label   Tag Meaning
-    # PER PER.NAM 名字（张三）
-    # PER.NOM 代称、类别名（穷人）
-    # LOC LOC.NAM 特指名称（紫玉山庄）
-    # LOC.NOM 泛称（大峡谷、宾馆）
-    # GPE GPE.NAM 行政区的名称（北京）
-    # ORG ORG.NAM 特定机构名称（通惠医院）
-    # ORG.NOM 泛指名称、统称（文艺公司）
-    # 原始标注 老百姓PER.NOM 新乡GPE.NAM
-    # gpt-3.5-turbo [{"name": "老百姓", "type": "代称", "start": 0, "end": 4}, {"name": "新乡新闻网", "type": "组织机构", "start": 4, "end": 10}, {"name": "新乡", "type": "行政区", "start": 12, "end": 14}, {"name": "天气", "type": "自然现象", "start": 14, "end": 16}]
-    # ERNIE-UIE {"text":"老百姓心新乡新闻网话说这几天新乡天气还好吧偷笑","result":[{"行政区":[{"text":"新乡","start":4,"end":6,"probability":0.589552328738506}]}]}
-
