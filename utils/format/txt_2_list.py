276c1a415765e9362c4f3d8fb8a9bd5b35fe0346
maxmon <541182180@qq.com>
2023-05-11 00:32:37 +0800

    feat(文本分类): 支持文本转数组，分类返回数组

---
+++ utils/format/txt_2_list.py
@@ -0,0 +1,11 @@
+import re
+
+def txt_2_list(txt):
+  split_token = r'[ ,、，;；《》<>]'
+  rm_token = r'["\'”“‘’。.！!？? 【】\[\]]'
+    
+  arr = re.split(split_token, txt)
+  arr = [re.sub(rm_token, '', item) for item in arr if item != '']
+  # 从大到小排序
+  arr.sort(key=lambda x: len(x), reverse=True)
+  return arr
