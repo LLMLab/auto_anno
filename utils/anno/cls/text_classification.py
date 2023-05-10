cceeffd989c780a4434212e595e18ad68dbebfb5
maxmon <541182180@qq.com>
2023-05-11 01:42:34 +0800

    feat(文本分类): 支持输入已标注数据来 few-shot 提升效果

--- utils/anno/cls/text_classification.py
+++ utils/anno/cls/text_classification.py
@@ -8,8 +8,9 @@ from utils.format.txt_2_list import txt_2_list
 # Set up your API key
 openai.api_key = openai_key
 
-def text_classification(src_txt, type_arr):
-    user = f"你是一个聪明而且有百年经验的文本分类器. 你的任务是从一段文本里面提取出相应的分类结果签。你的回答必须用统一的格式。文本用```符号分割。分类类型保存在一个数组里{type_arr}\n输入|```{src_txt}```输出|"
+def text_classification(src_txt, type_arr, history=[]):
+    history_txt = ''.join([f'输入|```{q}```输出|{a}\n' for q, a in history])
+    user = f"你是一个聪明而且有百年经验的文本分类器. 你的任务是从一段文本里面提取出相应的分类结果签。你的回答必须用统一的格式。文本用```符号分割。分类类型保存在一个数组里{type_arr}\n{history_txt}输入|```{src_txt}```输出|"
     # Call the OpenAI API
     completion = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
@@ -31,13 +32,18 @@ def text_classification(src_txt, type_arr):
 
 if __name__ == '__main__':
     # type_arr = ['好评', '差评']
-    type_arr_txt = "是差评、不是差评"
+    # type_arr_txt = "是差评、不是差评"
+    type_arr_txt = "天气查询、股票查询、其他"
     type_arr = txt_2_list(type_arr_txt)
     txts = [
         '这个商品真不错',
         '用着不行',
-        '没用过这么好的东西'
+        '没用过这么好的东西',
+        # '今天天气怎么样',
+    ]
+    history = [
+        ['这个商品真不错', ['其他']],
     ]
     for txt in txts:
-        result = text_classification(txt, type_arr)
+        result = text_classification(txt, type_arr, history)
         print(txt, result)
