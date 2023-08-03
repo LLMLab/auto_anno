dc08ae728120bc5e2a6a63a075ca3588d872d0c1
maxmon <541182180@qq.com>
2023-08-04 00:25:42 +0800

    feat: 支持文心一言 embedding 接口计算

--- utils/api/yiyan_api.py
+++ utils/api/yiyan_api.py
@@ -18,6 +18,18 @@ def en2cn_yiyan(prompt):
     result=chat_yiyan(Q_motif)
     return result
 
+def emb_yiyan(prompts):
+    if str(type(prompts)) == "<class 'str'>":
+        prompts = [prompts]
+    # curl 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=[API Key]&client_secret=[Secret Key]'
+    access_token = config['yiyan']['access_token']
+    url = f'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/embeddings/embedding-v1?access_token={access_token}'
+    result = requests.post(url, json={
+        "input": prompts
+    })
+    return [d['embedding'] for d in result.json()['data']]
+
 if __name__ == '__main__':
     print(chat_yiyan('你能做什么'))
     print(en2cn_yiyan('hello world'))
+    print(emb_yiyan(['hello world']))
