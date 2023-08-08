069157b57db22c06a06fd70fb076ea0c1ada53ac
maxmon <541182180@qq.com>
2023-08-08 22:55:30 +0800

    refactor: 当类别中间有换行时，类别按换行拆分

--- utils/format/txt_2_list.py
+++ utils/format/txt_2_list.py
@@ -1,10 +1,13 @@
 import re
 
 def txt_2_list(txt):
-  split_token = r'[ ,、，;；《》<>\n]'
+  txt = txt.strip()
+  if '\n' in txt:
+    arr = txt.split('\n')
+  else:
+    split_token = r'[ ,、，;；《》<>]'
+    arr = re.split(split_token, txt)
   rm_token = r'["\'”“‘’。.！!？? 【】\[\]]'
-    
-  arr = re.split(split_token, txt)
   arr = [re.sub(rm_token, '', item) for item in arr if item != '']
   # 从大到小排序
   arr.sort(key=lambda x: len(x), reverse=True)
