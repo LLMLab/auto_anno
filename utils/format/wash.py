aa47587a18bb88b88c9272a4617ecd5c5b9a7487
maxmon <541182180@qq.com>
2023-08-09 21:27:17 +0800

    feat: 支持全角转半角

--- utils/format/wash.py
+++ utils/format/wash.py
@@ -15,9 +15,26 @@ def wash_idcard(txt):
     washed_txt = re.sub(r'(\d{3})(\d{13})([\dXx]{2})', rf'\1{star}\3', txt)
     return washed_txt
 
+# 参考地址 https://www.jianshu.com/p/152e081fec1b
+def q_2_b(uchar):
+    """单个字符 全角转半角"""
+    inside_code = ord(uchar)
+    if inside_code == 0x3000:
+        inside_code = 0x0020
+    else:
+        inside_code -= 0xfee0
+    if inside_code < 0x0020 or inside_code > 0x7e: #转完之后不是半角字符返回原来的字符
+        return uchar
+    return chr(inside_code)
+
+def wash_q_2_b(txt):
+    """把字符串全角转半角"""
+    return "".join([q_2_b(uchar) for uchar in txt])
+
 if __name__ == '__main__':
     print(wash_tel('12345678901'))
     print(wash_tel('手机号：123-4567-8901'))
     print(wash_idcard('123456789012345678'))
     print(wash_idcard('我的身份证是33068119901214567X'))
     print(wash_tel('我的身份证是33068119901214567X'))
+    print(wash_q_2_b('＂第　３　题，Ａ选项是对的．＂'))
