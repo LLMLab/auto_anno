c834a7f5eecbb25fce6f6b89b652d8e3f03f901c
maxmon <541182180@qq.com>
2023-09-25 23:17:14 +0800

    feat: pypi 添加仓库相关信息

--- setup.py
+++ setup.py
@@ -3,8 +3,30 @@ from setuptools import setup, find_packages
 
 setup(
     name='auto_anno_2',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
+    # 仓库地址
+    url='https://github.com/LLMLab/auto_anno',
+    # 作者
+    author="LLMLab",
+    # 作者邮箱
+    author_email="541182180@qq.com",
+    # 描述
+    description='auto_anno_2',
+    # 项目主页
+    project_urls={
+        "Bug Tracker": "https://github.com/LLMLab/auto_anno",
+        "Documentation": "https://github.com/LLMLab/auto_anno",
+        "Source Code": "https://github.com/LLMLab/auto_anno",
+    },
+    # 项目分类
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    # 关键字
+    keywords='auto_anno_2',
+    # 安装依赖
     install_requires=[
         # list your project dependencies here
         'erniebot==0.3.1'
